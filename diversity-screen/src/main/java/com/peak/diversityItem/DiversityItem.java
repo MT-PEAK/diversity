297f3122811eeb47f39468cfe6c871c2a8c8267f
thatoneaiguy <animations.by.alby@gmail.com>
2026-03-21 22:41:27 +0000

    modules wip

---
+++ diversity-screen/src/main/java/com/peak/diversityItem/DiversityItem.java
@@ -0,0 +1,11 @@
+package com.peak.diversityItem;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DiversityItem implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ diversity-screen/src/main/java/com/peak/diversityItem/client/DiversityItemClient.java
@@ -0,0 +1,10 @@
+package com.peak.diversityItem.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DiversityItemClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
