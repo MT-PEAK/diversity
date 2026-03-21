30d5b4de67ff0878e28895b683a16e051e67a530
thatoneaiguy <animations.by.alby@gmail.com>
2026-03-21 22:42:04 +0000

    i forgot core

---
+++ diversity-core/src/main/java/com/peak/diversityCore/DiversityCore.java
@@ -0,0 +1,10 @@
+package com.peak.diversityCore;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DiversityCore implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ diversity-core/src/main/java/com/peak/diversityCore/client/DiversityCoreClient.java
@@ -0,0 +1,10 @@
+package com.peak.diversityCore.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DiversityCoreClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
